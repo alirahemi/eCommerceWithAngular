bbfad3abe0ec5a782726c94971b55a37fa0e7fae
Ali Rahemi <deutschlandmyhome@gmail.com>
2020-06-16 23:26:13 +0200

    section14 search for products by keyword

--- 03-frontend/angular-ecommerce/src/app/services/product.service.ts
+++ 03-frontend/angular-ecommerce/src/app/services/product.service.ts
@@ -21,6 +21,17 @@ export class ProductService {
     // built URL based on category id
     const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
 
+    return this.getProducts(searchUrl);
+  }
+
+  seachProducts(theKeyword: string): Observable<Product[]> {
+    // built URL based on keyword
+    const searchUrl = `${this.baseUrl}/search/findByNameContaining?name=${theKeyword}`;
+
+    return this.getProducts(searchUrl);
+  }
+
+  private getProducts(searchUrl: string): Observable<Product[]> {
     return this.httpClient.get<GetResponseProducts>(searchUrl).pipe(
       map(response => response._embedded.products)
     );
